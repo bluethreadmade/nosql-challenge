5d8bddc3d0e779186060df3d8f90a4cce3fac2a4
Megan McNeill <mcneim2@gmail.com>
2024-07-28 21:46:16 -0400

    added friendCount virtual to uesrname model

--- models/Username.js
+++ models/Username.js
@@ -1,24 +1,34 @@
 const { Schema } = require("mongoose");
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 // Schema to create User model
 const usernameSchema = new Schema({
   username: {
     type: String,
     unique: true,
     required: true,
-    // trimmed
+    trim: true,
   },
   email: {
     type: String,
     unique: true,
     required: true,
-    // must be a valid email
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
-  thoughts: {
-    // array of _id values referencing Thought model
-  },
-  friends: {
-    // array of _id values referencing User model
-  },
-  // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+  // array of _id values referencing Thought model
+  thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought'}],
+      
+  // array of _id values referencing User model
+  friends:[{ type: Schema.Types.ObjectId, ref: 'User'}],
+
 });
+
+// Virtual called friendCount that retrieves the length of the user's friends array field on query.
+usernameSchema.virtual('friendCount').get(function() {
+    return this.friends.length();
+})
